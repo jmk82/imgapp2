067004a78cba3e249d56df09607a6997e1930b44
Juha-Mikko Koskela <juhamikko@gmail.com>
2016-02-24 21:26:34 +0200

    remove adding /image to user filenames

--- public/app/controllers/user_ctrl.js
+++ public/app/controllers/user_ctrl.js
@@ -1,8 +1,8 @@
 App.controller('UserCtrl', function($scope, $http, $routeParams) {
   $http.get('/users/' + $routeParams.id).success(function (user) {
-    user.Images.forEach(function (image) {
+    /*user.Images.forEach(function (image) {
       image.filename = '/images/' + image.filename;
-    });
+    });*/
     $scope.user = user;
   });
 });
