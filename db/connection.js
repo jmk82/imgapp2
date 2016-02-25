7b7ba9da4af9bc2abd7721c45ce34eb716ec34f9
Juha-Mikko Koskela <juhamikko@gmail.com>
2016-02-26 00:13:45 +0200

    change to use config var for db

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://yddtxpwqkzshup:3K_HIdaDK6-rNSe_IyWLqsyNIE@ec2-184-73-253-4.compute-1.amazonaws.com:5432/d83dht2n4kph35', {
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
