b614511440452e42c4e7801adf36b91788d3f640
Juha-Mikko Koskela <juhamikko@gmail.com>
2016-02-28 17:56:27 +0200

    disable http request caching to make IE/Edge work

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,13 @@
 var App = angular.module('ImgApp', ['ngRoute'])
 
-App.config(['$routeProvider', function ($routeProvider) {
+App.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+  $httpProvider.defaults.cache = false;
+  if (!$httpProvider.defaults.headers.get) {
+    $httpProvider.defaults.headers.get = {};
+  }
+  // disable IE ajax request caching
+  $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+
   var auth = {
     userLoggedIn: function($rootScope, $http) {
       return $http.get('/users/logged-in').success(function(user) {
